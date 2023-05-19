a2d641d6fbdd077e7e886fa8f4bd1794a86952fe
Sumiyaakhi <sumiya.akhi793@gmail.com>
2023-05-20 00:54:53 +0600

    sub category data added

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
+const disneyPrincess = require('./DisneyPrincess.json');
+const frozen = require('./FrozenDoll.json');
+const animation = require('./AnimationDoll.json')
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,7 +13,17 @@ app.use(cors());
 app.use(express.json());
 
 
+app.get('/disneyPrincess',(req,res)=>{
+    res.send(disneyPrincess)
+})
 
+app.get('/animation', (req,res)=>{
+    res.send(animation);
+})
+
+app.get('/frozen',(req,res)=>{
+    res.send(frozen);
+})
 
 
 
